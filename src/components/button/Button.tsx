7578a3fb4d7895704345d0a1a91b23f95cb5708c
marcjordana <mjordana@alvic.net>
2025-02-20 11:37:16 +0100

    Initial commit

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import { ButtonProps } from "./types";
+
+import { StyledButton } from "./styles";
+
+const Button: React.FC<ButtonProps> = ({
+  size,
+  primary,
+  disabled,
+  text,
+  onClick,
+  ...props
+}) => {
+  return (
+    <StyledButton
+      type="button"
+      onClick={onClick}
+      primary={primary}
+      disabled={disabled}
+      size={size}
+      {...props}
+    >
+      {text}
+    </StyledButton>
+  );
+};
+
+export default Button;
